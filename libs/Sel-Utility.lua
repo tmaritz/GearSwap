cc031c4cc469da3e8409979c8572a9b1e26172cf
KalesAndRancor <some___guy@hotmail.com>
2025-11-26 10:58:05 -0600

    Change step check to check the target's model size.

--- libs/Sel-Utility.lua
+++ libs/Sel-Utility.lua
@@ -1348,7 +1348,7 @@ function check_abilities(spell, spellMap, eventArgs)
 				return true
 			end
 		elseif spell.type == 'Step' then
-			if player.status == 'Idle' and windower.ffxi.get_ability_recasts()[220] < latency and spell.target and spell.target.valid_target and spell.target.spawn_type == 16 and spell.target.distance < (3.2 + player.target.model_size) and player.tp > 99 then
+			if player.status == 'Idle' and windower.ffxi.get_ability_recasts()[220] < latency and spell.target and spell.target.valid_target and spell.target.spawn_type == 16 and spell.target.distance < (3.2 + spell.target.model_size) and player.tp > 99 then
 				packets.inject(packets.new('outgoing', 0x1a, {
 					['Target'] = spell.target.id,
 					['Target Index'] = spell.target.index,
